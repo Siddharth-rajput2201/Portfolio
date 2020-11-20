b007d25dcf507bde4d3dd8e81427dad9c7649086
Hush <rajputsiddharth18@gmail.com>
2020-11-20 21:19:35 +0530

    Updated download url for Home price predictor app

--- lib/Body/projectsPageBody.dart
+++ lib/Body/projectsPageBody.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/Widgets/FadeAnimation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Panel extends StatelessWidget {
@@ -128,9 +129,9 @@ class _projectsPageBodyState extends State<projectsPageBody> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Panel(projectName : 'HOME PRICE PREDICTOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/homepricepredictorapplogo.png', githuburl: 'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',downloadurl: 'https://drive.google.com/file/d/1pWFcpwIIj6a053jwYIEXxMq0s-gauTlv/view?usp=sharing'),
-                Panel(projectName : 'COVID 19 TRACKER', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/coivd19tracker.png', githuburl: 'https://github.com/Siddharth-rajput2201/Covid19_Tracker',downloadurl: 'https://drive.google.com/file/d/1X5gDFku0dlgBINn65jekwf8vb7OF_-SY/view?usp=sharing'),
-                Panel(projectName : 'BMI CALCULATOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/bmicalculator.png', githuburl: 'https://github.com/Siddharth-rajput2201/Dice_App',downloadurl: 'https://drive.google.com/file/d/19scktoOD6_-A_86pEnC7aVW0SVlAKWEm/view?usp=sharing'),
+                FadeAnimation(1.0,Panel(projectName : 'HOME PRICE PREDICTOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/homepricepredictorapplogo.png', githuburl: 'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',downloadurl: 'https://drive.google.com/file/d/1EDV8ZL4WmvAdx0sW56Y_naTP09zZP8ZD/view?usp=sharing'),),
+                FadeAnimation(1.0,Panel(projectName : 'COVID 19 TRACKER', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/coivd19tracker.png', githuburl: 'https://github.com/Siddharth-rajput2201/Covid19_Tracker',downloadurl: 'https://drive.google.com/file/d/1X5gDFku0dlgBINn65jekwf8vb7OF_-SY/view?usp=sharing'),),
+                FadeAnimation(1.0,Panel(projectName : 'BMI CALCULATOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/bmicalculator.png', githuburl: 'https://github.com/Siddharth-rajput2201/Dice_App',downloadurl: 'https://drive.google.com/file/d/19scktoOD6_-A_86pEnC7aVW0SVlAKWEm/view?usp=sharing'),),
               ],
             ),
           ],
@@ -139,8 +140,9 @@ class _projectsPageBodyState extends State<projectsPageBody> {
       } else {
         return Column(
           children: [
-            Panel(downloadurl: 'https://drive.google.com/file/d/1pWFcpwIIj6a053jwYIEXxMq0s-gauTlv/view?usp=sharing',projectName : 'HOME PRICE PREDICTOR', width: constraints.biggest.width / 1.15 ,imagepath: 'assets/images/homepricepredictorapplogo.png', githuburl: 'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',
-            ),
+            FadeAnimation(1.0,Panel(projectName : 'HOME PRICE PREDICTOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/homepricepredictorapplogo.png', githuburl: 'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',downloadurl: 'https://drive.google.com/file/d/1EDV8ZL4WmvAdx0sW56Y_naTP09zZP8ZD/view?usp=sharing'),),
+            FadeAnimation(1.0,Panel(projectName : 'COVID 19 TRACKER', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/coivd19tracker.png', githuburl: 'https://github.com/Siddharth-rajput2201/Covid19_Tracker',downloadurl: 'https://drive.google.com/file/d/1X5gDFku0dlgBINn65jekwf8vb7OF_-SY/view?usp=sharing'),),
+            FadeAnimation(1.0,Panel(projectName : 'BMI CALCULATOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/bmicalculator.png', githuburl: 'https://github.com/Siddharth-rajput2201/Dice_App',downloadurl: 'https://drive.google.com/file/d/19scktoOD6_-A_86pEnC7aVW0SVlAKWEm/view?usp=sharing'),),
           ],
         );
       }
