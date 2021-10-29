4e912cd5fe75a35a86ba6612b8003b63377b2cbf
Siddharth Rajput <34578773+Siddharth-rajput2201@users.noreply.github.com>
2021-10-29 14:34:36 +0530

    Add files via upload
    Removed BMI Calculator

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "35b33b0f1377a5eafd3570650ccb7592",
-"assets/NOTICES": "8ae6c03581affc19af961fa0bfe411e8",
+  "main.dart.js": "2fa314916160f200af571de24dd48bbd",
+"assets/NOTICES": "1c74a4dd0340555e41155dfc29585ad6",
 "assets/assets/images/coivd19tracker.png": "4abfa0a9ab0741ca5501053fd355e584",
 "assets/assets/images/instagram.png": "03f40b67b63d9c1c1a5a792109bbc699",
 "assets/assets/images/downloadicon.png": "8980dcbf6406a2dd29278bdce4f5d33a",
@@ -16,10 +16,10 @@ const RESOURCES = {
 "assets/assets/images/profile_photo.jpeg": "e6b30dba62df312f77d26c4479ecfd25",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/AssetManifest.json": "d6f1b4460269f1bee39bae0972d96b19",
-"index.html": "4385e6c5315d9582e89f6fad0625ca8e",
-"/": "4385e6c5315d9582e89f6fad0625ca8e",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/AssetManifest.json": "18d8335ade88b801c2c36406c8b1e9ba",
+"index.html": "ce5f21c557c3ee3039d415d505c2c19c",
+"/": "ce5f21c557c3ee3039d415d505c2c19c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -42,7 +42,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -168,7 +168,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
