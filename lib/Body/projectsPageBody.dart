55844a14e53a831585bee1196882790805c788bd
Hush <siddharth@rapidalley.com>
2021-10-29 15:25:49 +0530

    Removed Link Error

--- lib/Body/projectsPageBody.dart
+++ lib/Body/projectsPageBody.dart
@@ -4,29 +4,32 @@ import 'package:portfolio/Widgets/FadeAnimation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Panel extends StatelessWidget {
-
   void _urllauncher(String url) async {
     await launch("$url");
   }
-
-
-
 
   final String projectName;
   final String imagepath;
   final double width;
   final String githuburl;
   final String downloadurl;
-  const Panel({Key key, this.projectName ,this.width , this.imagepath ,this.githuburl,this.downloadurl}) : super(key: key);
+  const Panel(
+      {Key key,
+      this.projectName,
+      this.width,
+      this.imagepath,
+      this.githuburl,
+      this.downloadurl})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: GestureDetector(
-        onTap: (){},
+        onTap: () {},
         child: Container(
           width: width,
-          decoration : BoxDecoration(
+          decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(15),
               boxShadow: [
@@ -47,36 +50,48 @@ class Panel extends StatelessWidget {
                       padding: const EdgeInsets.all(8.0),
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(10.0),
-                          child:Image.asset(imagepath,fit: BoxFit.contain)
-                      ),
+                          child: Image.asset(imagepath, fit: BoxFit.contain)),
                     ),
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Padding(
-                    padding: const EdgeInsets.only(left : 12.0 , right: 12.0),
-                    child: Divider(thickness: 2,color: Colors.grey,),
+                    padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                    child: Divider(
+                      thickness: 2,
+                      color: Colors.grey,
+                    ),
                   ),
                   Center(
                     child: Padding(
-                      padding: const EdgeInsets.only(left : 12.0 , right: 12.0),
-                      child: RichText(text: TextSpan(
-                          text : projectName,
-                          style: GoogleFonts.barlow(textStyle: TextStyle(color: Colors.black54) ,fontSize: 26 ,fontWeight: FontWeight.w800)
-                      ),
+                      padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                      child: RichText(
+                        text: TextSpan(
+                            text: projectName,
+                            style: GoogleFonts.barlow(
+                                textStyle: TextStyle(color: Colors.black54),
+                                fontSize: 26,
+                                fontWeight: FontWeight.w800)),
                       ),
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(left : 12.0 , right: 12.0),
-                    child: Divider(thickness: 2,color: Colors.grey,),
+                    padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                    child: Divider(
+                      thickness: 2,
+                      color: Colors.grey,
+                    ),
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       GestureDetector(
-                        onTap: (){
-                           _urllauncher(githuburl);
+                        onTap: () {
+                          _urllauncher(githuburl);
                         },
                         child: Image.asset(
                           "assets/images/github.png",
@@ -88,7 +103,7 @@ class Panel extends StatelessWidget {
                         width: 10,
                       ),
                       GestureDetector(
-                        onTap: (){
+                        onTap: () {
                           _urllauncher(downloadurl);
                         },
                         child: Image.asset(
@@ -112,7 +127,6 @@ class Panel extends StatelessWidget {
   }
 }
 
-
 // ignore: camel_case_types
 class projectsPageBody extends StatefulWidget {
   @override
@@ -130,9 +144,39 @@ class _projectsPageBodyState extends State<projectsPageBody> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FadeAnimation(1.0,Panel(projectName : 'HOME PRICE PREDICTOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/homepricepredictorapplogo.png', githuburl: 'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',downloadurl: 'https://drive.google.com/file/d/1EDV8ZL4WmvAdx0sW56Y_naTP09zZP8ZD/view?usp=sharing'),),
-                //FadeAnimation(1.0,Panel(projectName : 'COVID 19 TRACKER', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/coivd19tracker.png', githuburl: 'https://github.com/Siddharth-rajput2201/Covid19_Tracker',downloadurl: 'https://drive.google.com/file/d/1X5gDFku0dlgBINn65jekwf8vb7OF_-SY/view?usp=sharing'),),
-                //FadeAnimation(1.0,Panel(projectName : 'BMI CALCULATOR', width: constraints.biggest.width / 4.5 ,imagepath: 'assets/images/bmicalculator.png', githuburl: 'https://github.com/Siddharth-rajput2201/Dice_App',downloadurl: 'https://drive.google.com/file/d/19scktoOD6_-A_86pEnC7aVW0SVlAKWEm/view?usp=sharing'),),
+                FadeAnimation(
+                  1.0,
+                  Panel(
+                      projectName: 'HOME PRICE PREDICTOR',
+                      width: constraints.biggest.width / 4.5,
+                      imagepath: 'assets/images/homepricepredictorapplogo.png',
+                      githuburl:
+                          'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',
+                      downloadurl:
+                          'https://drive.google.com/file/d/1EDV8ZL4WmvAdx0sW56Y_naTP09zZP8ZD/view?usp=sharing'),
+                ),
+                FadeAnimation(
+                  1.0,
+                  Panel(
+                      projectName: 'COVID 19 TRACKER',
+                      width: constraints.biggest.width / 4.5,
+                      imagepath: 'assets/images/coivd19tracker.png',
+                      githuburl:
+                          'https://github.com/Siddharth-rajput2201/Covid19_Tracker',
+                      downloadurl:
+                          'https://drive.google.com/file/d/1X5gDFku0dlgBINn65jekwf8vb7OF_-SY/view?usp=sharing'),
+                ),
+                FadeAnimation(
+                  1.0,
+                  Panel(
+                      projectName: 'BMI CALCULATOR',
+                      width: constraints.biggest.width / 4.5,
+                      imagepath: 'assets/images/bmicalculator.png',
+                      githuburl:
+                          'https://github.com/Siddharth-rajput2201/BMI-Calculaor-App',
+                      downloadurl:
+                          'https://drive.google.com/file/d/19scktoOD6_-A_86pEnC7aVW0SVlAKWEm/view?usp=sharing'),
+                ),
               ],
             ),
           ],
@@ -141,9 +185,39 @@ class _projectsPageBodyState extends State<projectsPageBody> {
       } else {
         return Column(
           children: [
-            FadeAnimation(1.0,Panel(projectName : 'HOME PRICE PREDICTOR', width: constraints.biggest.width / 1.15 ,imagepath: 'assets/images/homepricepredictorapplogo.png', githuburl: 'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',downloadurl: 'https://drive.google.com/file/d/1vaR-AUEsCS6dOKGPhrRFzzh3YsXs28CV/view?usp=sharing'),),
-            FadeAnimation(1.0,Panel(projectName : 'COVID 19 TRACKER', width: constraints.biggest.width / 1.15 ,imagepath: 'assets/images/coivd19tracker.png', githuburl: 'https://github.com/Siddharth-rajput2201/Covid19_Tracker',downloadurl: 'https://drive.google.com/file/d/1X5gDFku0dlgBINn65jekwf8vb7OF_-SY/view?usp=sharing'),),
-            FadeAnimation(1.0,Panel(projectName : 'BMI CALCULATOR', width: constraints.biggest.width / 1.15 ,imagepath: 'assets/images/bmicalculator.png', githuburl: 'https://github.com/Siddharth-rajput2201/BMI-Calculaor-App',downloadurl: 'https://drive.google.com/file/d/19scktoOD6_-A_86pEnC7aVW0SVlAKWEm/view?usp=sharing'),),
+            FadeAnimation(
+              1.0,
+              Panel(
+                  projectName: 'HOME PRICE PREDICTOR',
+                  width: constraints.biggest.width / 1.15,
+                  imagepath: 'assets/images/homepricepredictorapplogo.png',
+                  githuburl:
+                      'https://github.com/Siddharth-rajput2201/Home_Price_Predictor_App',
+                  downloadurl:
+                      'https://drive.google.com/file/d/1vaR-AUEsCS6dOKGPhrRFzzh3YsXs28CV/view?usp=sharing'),
+            ),
+            FadeAnimation(
+              1.0,
+              Panel(
+                  projectName: 'COVID 19 TRACKER',
+                  width: constraints.biggest.width / 1.15,
+                  imagepath: 'assets/images/coivd19tracker.png',
+                  githuburl:
+                      'https://github.com/Siddharth-rajput2201/Covid19_Tracker',
+                  downloadurl:
+                      'https://drive.google.com/file/d/1X5gDFku0dlgBINn65jekwf8vb7OF_-SY/view?usp=sharing'),
+            ),
+            FadeAnimation(
+              1.0,
+              Panel(
+                  projectName: 'BMI CALCULATOR',
+                  width: constraints.biggest.width / 1.15,
+                  imagepath: 'assets/images/bmicalculator.png',
+                  githuburl:
+                      'https://github.com/Siddharth-rajput2201/BMI-Calculaor-App',
+                  downloadurl:
+                      'https://drive.google.com/file/d/19scktoOD6_-A_86pEnC7aVW0SVlAKWEm/view?usp=sharing'),
+            ),
           ],
         );
       }
